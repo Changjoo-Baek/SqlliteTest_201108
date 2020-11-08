44ea6e5fa8eb8b45f49fd4459be78b7c69b352b2
bucephalus41 <bucephalus41@gmail.com>
2020-11-08 18:50:16 +0900

    dbhelper 끝

---
+++ app/src/main/java/com/zzazzu/sqllitetest_201108/DBHelper.kt
@@ -0,0 +1,101 @@
+package com.zzazzu.sqllitetest_201108
+
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+@Suppress("DEPRECATION")
+class DBHelper(context: Context?, name:String?, factory:SQLiteDatabase.CursorFactory?, version:Int) :
+        SQLiteOpenHelper(context,name,factory,version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        if(db !=null) {
+            db.execSQL(
+                "CREATE TABLE MEMBER(NAME TEST,"+ "ID TEXT, PASSWORD TEXT, PASSWORD_OK TEXT, PHONE TEXT, EMAIL TEXT, ADDRESS TEXT, LEVEL TEXT);"
+            )
+
+        }
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
+
+    fun insert(
+        name: String, id: String, password: String, password_ok: String, phone: String, email: String, address: String, level : String){
+        var db: SQLiteDatabase = writableDatabase
+
+        db.execSQL(
+            "INSERT INTO MEMBER VALUES('" + name + "'" + ".'" + id + "'" + ", '" + password + "'" + ", '" + password_ok + "'" + ", '" + phone + "'" + ", '" + email + "'" + ", '" + address + "'" + " , '" + level + "');"
+
+        )
+        db.close()
+    }
+
+    fun delete(name : String) {
+        var db: SQLiteDatabase = writableDatabase
+
+        db.execSQL("DELETE FROM MEMBER WHERE NAME = '"+ name + "';")
+        db.close()
+    }
+
+    fun update(
+        name: String, password: String, password_ok: String, phone: String, email: String, address: String, level : String){
+        var db: SQLiteDatabase = writableDatabase
+
+        db.execSQL(
+            "UPDATE MEMBER SET PASSWORD = " + "'" + password + "'" + ", PASSWORD_OK = '" + password_ok = "'" + ", PHONE = '" + phone + "'" + ", EMAIL = '" + email + "'" + ", ADDRESS = '" + address + "'" + ", LEVEL = '" + level + "'" + "WHERE NAME = '" + name + "';")
+
+        db.close()
+    }
+
+    fun getResult(): String {
+        var db: SQLiteDatabase = readableDatabase
+        var result: String = ""
+
+        var cursor: Cursor = db.rawQuery("SELECT * FROM MEMBER", null)
+        while (cursor.moveToNext()) {
+            result += (cursor.getString(0)
+                    + ":"
+                    + cursor.getString(1)
+                    + ":"
+                    +cursor.getString(2)
+                    + ":"
+                    +cursor.getString(3)
+                    + ":"
+                    + cursor.getString(4)
+                    + ":"
+                    +cursor.getString(5)
+                    + ":"
+                    + cursor.getString(6)
+                    + ":"
+                    +cursor.getString(7)
+                    + "\n")
+
+        }
+
+        return result
+    }
+
+    fun getResult1(ID: String, PASSWORD:String):Boolean {
+        var db: SQLiteDatabase = readableDatabase
+        var result: String = ""
+
+        var cursor: Cursor = db.rawQuery("SELECT ID, PASSWORD FROM MEMBER", null)
+        while (cursor.moveToNext()) {
+            result = (cursor.getString(0))
+            if (result.equals(ID)) {
+                if (cursor.getString(1).equals(PASSWORD)) {
+                    return true
+                    break
+                } else {
+                    return false
+                }
+            } else {
+            }
+        }
+
+        return false
+    }
+}
--- app/src/main/java/com/zzazzu/sqllitetest_201108/MainActivity.kt
+++ app/src/main/java/com/zzazzu/sqllitetest_201108/MainActivity.kt
@@ -7,5 +7,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var dbHelper: DBHelper = DBHelper(applicationContext, "MEMBER.db", null, 1)
+
+        login_Btn.setOnClickListener {
+            if(dbHelper.getResult1(editText.getText().toString(), editText2.getText().toString()) == true){
+                toast("로그인 성공")
+                startActivity<LoginActivity>()
+            }else{
+                toast("로그인 실패")
+            }
+        }
+
+
+        register_Btn.setOnClickListener {
+            startActivity<RegisterActivity>()
+        }
+
+
     }
 }
